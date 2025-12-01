f2d3d0c7aa3658e9f4468591e3d15d4374bf7350
Miklos Marton <martonmiklosqdev@gmail.com>
2025-12-01 20:11:52 +0100

    Bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ SRC_DIR="${INPUT_PATH:-.}"
 
 echo "Running clang-tidy with std-prefix plugin..."
 clang-tidy \
-    -load=/usr/local/lib/clangTidyStdPrefixPlugin.so \
+    -load=/usr/local/lib/libclangTidyStdPrefixPlugin.so \
     $(find "$SRC_DIR" -name "*.cpp" -o -name "*.h")
